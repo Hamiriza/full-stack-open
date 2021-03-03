7f73aac3c026531e165b81643ab13873b4248958
Hamiriza Firdhan Ardyasta <hamirizafirdhanardyasta@itsss-MacBook-Pro.local>
2021-03-03 11:58:34 +0800

    pushing anecdotes and unicafe exercises

---
+++ part1/1.12-1.14=anecdotes/src/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(
+    Array.from({ length: anecdotes.length }, (undef) => 0)
+  );
+
+  const handleClick = () => setSelected(Math.floor(Math.random() * 6));
+  const handleVotes = () => {
+    let newVotes = [...votes];
+    newVotes[selected]++;
+    setVotes(newVotes);
+  };
+  const maxIndex = votes.indexOf(Math.max(...votes));
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {props.anecdotes[selected]}
+      <br />
+      has {votes[selected]} vote(s)
+      <br />
+      <button onClick={handleVotes}>vote</button>
+      <button onClick={handleClick}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      {props.anecdotes[maxIndex]}
+      <br />
+      has {votes[maxIndex]} vote(s)
+    </div>
+  );
+};
+
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
